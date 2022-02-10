6354cfdc9ea61c2bb5f181e381a1f08b4a886fa6
JamesMusembi <james.musembi@student.moringaschool.com>
2022-02-10 12:23:17 +0300

    patch:modified css and html and js

--- login.js
+++ login.js
@@ -1,29 +1,35 @@
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");     document.querySelector("#linkLogin"). addEventListener("click" , e =>{
+    const createAccountForm = document.querySelector("#createAccount");
+    document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
     });
-    document.querySelector("#linkCreateAccount"). addEventListener("click" , e =>{
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden");
-    });});
-    //validating user input
+    });
+});
+//validating user input
 var form = document.getElementById("login");
-function handleForm(event) { event.preventDefault(); }
-form.addEventListener('submit', handleForm);function validate(){
-    var username= document.getElementById("username").value;
+
+function handleForm(event) {
+    event.preventDefault();
+}
+form.addEventListener('submit', handleForm);
+
+function validate() {
+    var username = document.getElementById("username").value;
     console.log(username);
     var password = document.getElementById("password").value;
     console.log(password);
-    if(username == "Wendy" && password == "user"){
+    if (username == "Wendy" && password == "user") {
         alert("You are logged in.");
-        window.location.href="member.html";
+        window.location.href = "member.html";
         // return true;
-    }
- else{
-     alert("Invalid userName and Password")
- }
- 
+    } else {
+        alert("Invalid userName and Password")
+    };
+}

2704e9896779896e4eb0b94fa0d175350ab67d14
JamesMusembi <james.musembi@student.moringaschool.com>
2022-02-10 10:57:13 +0300

    patch:modified js

---
+++ login.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",() => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");     document.querySelector("#linkLogin"). addEventListener("click" , e =>{
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+    document.querySelector("#linkCreateAccount"). addEventListener("click" , e =>{
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });});
